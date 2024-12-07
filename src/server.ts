0e45e7f680f8f03c4add3918726b572cb99c2987
cardosojl <cardosojl@outlook.com.br>
2024-12-07 20:30:42 -0300

    add HOST value to start a server

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,9 @@ import broker from './broker';
 import { logger } from './config/logger';
 
 const port = process.env.PORT || 1883;
+const host = process.env.HOST as string || 'localhost';
 const server: Server = net.createServer(broker.handle);
 
-server.listen(port, () => {
+server.listen({ port, host }, () => {
     logger.info(`MQTT broker is running on port ${port}`);
 });
